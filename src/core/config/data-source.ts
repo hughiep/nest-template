bc0aa47d1591b98b1b930ba1d8335628fe6704af
Hiep Nguyen <hiepnv.wt@gmail.com>
2025-08-15 23:53:38 +0700

    refactor(data-source): remove default values for database connection options

--- src/core/config/data-source.ts
+++ src/core/config/data-source.ts
@@ -6,11 +6,11 @@ config(); // Load .env file
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_NAME || 'nest_db',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/migrations/*.js'],
   migrationsTableName: 'migrations',
